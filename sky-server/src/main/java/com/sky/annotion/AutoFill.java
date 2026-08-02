79a6db829d3c986ae0bdded2a2f1fe6668b1772a
hjhssupersg <3330288532@qq.com>
2026-08-02 11:39:06 +0800

    新增了“公共字段（create_time, update_time, create_user, update_user）自动填充”功能

---
+++ sky-server/src/main/java/com/sky/annotion/AutoFill.java
@@ -0,0 +1,18 @@
+package com.sky.annotion;
+
+import com.sky.enumeration.OperationType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义注解，用于标记需要进行 “公共字段自动填充 ”的方法
+ */
+@Target(ElementType.METHOD)//表示该注解可以用在方法上
+@Retention(RetentionPolicy.RUNTIME)//表示该注解在运行时仍然可用，可以通过反射获取到该注解的信息
+public @interface AutoFill {
+    //数据库操作类型：UPDATE、INSERT
+    OperationType value();
+}
---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,83 @@
+package com.sky.aspect;
+
+import com.sky.annotion.AutoFill;
+import com.sky.constant.AutoFillConstant;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+@Aspect
+@Component
+@Slf4j
+public class AutoFillAspect {
+    /**
+     * 切入点：拦截所有 mapper包下的类中，带有 @AutoFill注解的方法
+     */
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotion.AutoFill)")
+    public void autoFillPointCut() {
+    }
+
+    /**
+     * 前置通知：在切入点方法执行之前，执行自动填充公共字段的逻辑
+     */
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint) {
+        log.info("开始自动填充公共字段……");
+
+        //获取当前被拦截的方法上的数据库操作类型
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();//获取方法签名对象
+        AutoFill autoFill = methodSignature.getMethod().getAnnotation(AutoFill.class);//获取方法上的注解对象
+        OperationType operationType = autoFill.value();//获取注解的值，即数据库操作类型
+
+        //获取当前被拦截的方法的参数，即实体对象
+        Object[] args = joinPoint.getArgs();
+        if (args == null || args.length == 0) {
+            return;
+        }
+        Object entity = args[0];//获取第一个参数，即实体对象
+        //准备好要填充的公共字段的值
+        LocalDateTime now = LocalDateTime.now();
+        Long currentId = BaseContext.getCurrentId();
+
+        //根据当前不同的操作类型，通过反射为对应的属性赋值
+        if (operationType == OperationType.INSERT) {
+            //新增操作需要设置创建时间、修改时间、创建人、修改人
+            try {
+                Method setCreateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
+                Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                Method setCreateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+                Method setUpdateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+
+                //通过反射调用实体对象的setter方法，为公共字段赋值
+                setCreateTime.invoke(entity, now);
+                setUpdateTime.invoke(entity, now);
+                setCreateUser.invoke(entity, currentId);
+                setUpdateUser.invoke(entity, currentId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else if (operationType == OperationType.UPDATE) {
+            //更新操作只需要设置修改时间和修改人
+            try {
+                Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+                Method setUpdateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+
+                //通过反射调用实体对象的setter方法，为公共字段赋值
+                setUpdateTime.invoke(entity, now);
+                setUpdateUser.invoke(entity, currentId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
