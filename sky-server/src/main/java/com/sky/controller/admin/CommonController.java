8086d378809db0b43288a79857ddba2c6f54726a
hjhssupersg <3330288532@qq.com>
2026-08-04 10:47:16 +0800

    添加了“新增菜品”功能中的“文件上传”模块

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Slf4j
+/**
+ * 文件上传（上传到阿里云OSS对象存储服务平台）
+ * @param file
+ * @return
+ */
+public class CommonController {
+    @Autowired
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file) {
+        try {
+            log.info("文件上传：{}", file);
+            //原始文件名
+            String originalFileName = file.getOriginalFilename();
+            //原始文件名后缀
+            String extention = originalFileName.substring(originalFileName.lastIndexOf('.'));
+            //通过UUID构造新的文件名称（防止重复）
+            String objectName = UUID.randomUUID().toString() + extention;
+            //获取文件请求路径并上传至阿里云OSS
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
